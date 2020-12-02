a0c183c048f893a159627d03ea19773268b612a5
Sigurd Gartmann <sigurdga@sigurdga.no>
2020-12-02 23:51:43 +0100

    Works with the new frontend

--- background.js
+++ background.js
@@ -1,41 +1,43 @@
 const PROTOCOLS = ["http:", "https:"];
 
 function hasGoodUrl(url) {
-    var link = document.createElement('a');
-    link.href = url;
-    return PROTOCOLS.includes(link.protocol) && link.hostname !== 'rememberthelink.com';
+  var link = document.createElement("a");
+  link.href = url;
+  return (
+    PROTOCOLS.includes(link.protocol) && link.hostname !== "rememberthelink.com"
+  );
 }
 
 function initializePageAction(tab) {
-    if (hasGoodUrl(tab.url)) {
-        browser.pageAction.setIcon({
-            tabId: tab.id,
-            path: {
-                19: "icons/rememberthelink-19.png",
-                38: "icons/rememberthelink-38.png"
-            }
-        });
-        browser.pageAction.setTitle({
-            tabId: tab.id,
-            title: "Remember"
-        });
-        browser.pageAction.show(tab.id);
-    }
+  if (hasGoodUrl(tab.url)) {
+    chrome.pageAction.setIcon({
+      tabId: tab.id,
+      path: {
+        19: "icons/rememberthelink-19.png",
+        38: "icons/rememberthelink-38.png",
+      },
+    });
+    chrome.pageAction.setTitle({
+      tabId: tab.id,
+      title: "Remember",
+    });
+    chrome.pageAction.show(tab.id);
+  }
 }
 
-var gettingAllTabs = browser.tabs.query({});
-gettingAllTabs.then((tabs) => {
-    for (let tab of tabs) {
-        initializePageAction(tab);
-    }
+chrome.tabs.query({}, (tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
 });
 
 // Listen for any changes to the URL of any tab, and update the page action for that tab
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-    initializePageAction(tab);
+chrome.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
 });
 
-browser.pageAction.onClicked.addListener(function(tab) {
-    var url = 'https://rememberthelink.com/new?url=' + tab.url;
-    browser.tabs.update(tab.id, { url: url });
+chrome.pageAction.onClicked.addListener(function (tab) {
+  var url =
+    "https://rememberthelink.com/new?url=" + encodeURIComponent(tab.url);
+  chrome.tabs.update(tab.id, { url: url });
 });
